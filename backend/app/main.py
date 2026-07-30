5694c96f004ca4b5dce3617280115e0adb3d047c
TLDOOM <tjlansang123@gmail.com>
2026-07-31 02:00:25 +0800

    updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI,Depends
-from app.core.db import Base, engine,SessionLocal
-from app.v1.routes import user,projects,future
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
+
+from app.core.db import Base, engine, SessionLocal
 from app.models.projects import Projects
-from app.models.future import Future # example SQLAlchemy modell
+from app.models.future import Future
+from app.v1.routes import user, projects, future
 
 app = FastAPI(title="Portfolio Backend")
+
 
 def get_db():
     db = SessionLocal()
@@ -23,27 +25,29 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
-        "http://localhost:4173"
+        "http://localhost:4173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Create tables
+
 @app.on_event("startup")
 def on_startup():
     Base.metadata.create_all(bind=engine)
 
+
 app.include_router(user.router, prefix="/api/v1")
-app.include_router(projects.router, prefix ="/api/v1")
-app.include_router(future.router, prefix= "/api/v1")
+app.include_router(projects.router, prefix="/api/v1")
+app.include_router(future.router, prefix="/api/v1")
+
 
 @app.get("/projects")
 def read_project(db: Session = Depends(get_db)):
     return db.query(Projects).all()
 
+
 @app.get("/future")
 def read_future(db: Session = Depends(get_db)):
     return db.query(Future).all()
-
